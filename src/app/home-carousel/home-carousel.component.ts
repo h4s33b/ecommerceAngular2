3084f7faadacb589be14953cc6f3e3f22bc9efa4
h4s33b <haseeb.rehman@iunc.edu.pk>
2016-12-14 16:05:39 +0500

    Worked on home Screen, Added Slider html, ts and css, Added Sample Navbar

--- src/app/home-carousel/home-carousel.component.ts
+++ src/app/home-carousel/home-carousel.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeCarouselComponent implements OnInit {
 
+  sliderContent;
   constructor() { }
 
   ngOnInit() {
+    this.sliderContent = [{ "name": "Hello", "desc": "This is test.", "imgUrl": "images/home/girl1.jpg" },
+      { "name": "hello2", "desc": "This is test.", "imgUrl": "images/home/girl2.jpg" },
+      { "name": "hello3", "desc": "This is test.", "imgUrl": "images/home/girl3.jpg" }];
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: `
-    <div class="container text-center">
-		This is Home page
-        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-	</div>
-  `
+  templateUrl: './home.component.html'
 })
 export class HomeComponent { }
