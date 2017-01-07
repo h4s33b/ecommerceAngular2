e0009bbcc0e8a3c24efd752d78111426a640edd5
h4s33b <haseeb.rehman@iunc.edu.pk>
2017-01-07 12:09:36 +0500

    Added methods for Cart Service

--- src/app/shopping-cart/cart.service.ts
+++ src/app/shopping-cart/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Iitem} from '../interfaces/item';
-import {Observable} from 'rxjs/Observable';
+import { Iitem } from '../interfaces/item';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CartService {
@@ -11,13 +11,13 @@ export class CartService {
     }
 
     addCartItem(item: Iitem, quantity: number): void {
-        var isNewItem: Boolean = true;
-        this.items.forEach((x,index) => {
+        let isNewItem: Boolean = true;
+        this.items.forEach((x, index) => {
             if (x.productId && x.productId == item.productId) {
                 this.items[index].quantity = Number(quantity) + Number(this.items[index].quantity);
                 isNewItem = false;
             }
-        })
+        });
         if (isNewItem) {
             item.quantity = quantity;
             this.items.push(item);
@@ -27,4 +27,49 @@ export class CartService {
     getCartItems(): Observable<Iitem> {
         return Observable.from(this.items);
     }
-}
+
+    deleteCartItem(item: Iitem): void {
+        this.items.forEach((x, index) => {
+            if (x.productId && x.productId == item.productId) {
+                this.items.splice(index, 1);
+            }
+        });
+    }
+
+    incrementItemQuantity(item: Iitem, quantity: number): void {
+        this.items.forEach((x, index) => {
+            if (x.productId && x.productId == item.productId) {
+                this.items[index].quantity = Number(quantity) + Number(this.items[index].quantity);
+            }
+        });
+    }
+
+    decrementItemQuantity(item: Iitem, quantity: number): void {
+        this.items.forEach((x, index) => {
+            if (x.productId && x.productId == item.productId) {
+                this.items[index].quantity = Number(this.items[index].quantity) - Number(quantity);
+            }
+        });
+    }
+
+    getCartItemsCount(): number {
+        let totalItemsCount = 0;
+        if (this.items.length) {
+            this.items.forEach((x, index) => {
+                totalItemsCount += x.quantity;
+            });
+        }
+        return totalItemsCount;
+    }
+
+    getCartItemsPrice(): number {
+        let totalCartItemsPrice = 0;
+        if (this.items.length) {
+            this.items.forEach((x, index) => {
+                totalCartItemsPrice += x.quantity * x.price;
+            });
+        }
+        return totalCartItemsPrice;
+    }
+
+};
